f083103c629e8abe401c15b7efdef7e8ad0b91da
leroyBoys <wlvxiaohui@163.com>
2018-03-16 19:18:11 +0800

    init2

---
+++ application/app/config/pid_config.php
@@ -0,0 +1,2 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['ali_pid'] = 'mm_97928704_43428463_322620247';
---
+++ application/app/config/site_config.php
@@ -0,0 +1,17 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------
+| site_config
+| -------------------------------------------------------------------
+*/
+$config['sys_site_name'] = '搜客淘宝客程序API自动更新自动采集版-';
+$config['sys_site_title'] = '搜客淘宝客程序API自动更新自动采集版-配置淘点金-加载爱搭配';
+$config['sys_site_logo'] = 'uploadfiles/images/20150426/e937fe89010236e009749aca14cc60e0.png';
+$config['sys_site_keyword'] = '搜客淘宝客程序API自动更新自动采集版-配置淘点金-加载爱搭配';
+$config['sys_site_description'] = '搜客淘宝客程序API自动更新自动采集版-配置淘点金-加载爱搭配';
+$config['sys_site_copyright'] = '© 2016 莎莎源码论坛【http://bbs.sasadown.cn】 保留所有权利';
+$config['sys_cache_time'] = '0';
+$config['sys_is_power'] = '';
+$config['sys_tongji'] = '';
+/* End of file site_config.php */
+/* Location: ./system/application/config/site_config.php */
