06e8fbbb1128044ccdb8049cf1c307d3a7f32ccf
leroyBoys <wlvxiaohui@163.com>
2018-03-28 14:16:09 +0800

    init2

--- view/ttae/header.php
+++ view/ttae/header.php
@@ -74,7 +74,7 @@
                     </div>
      			  <input placeholder="试试搜索精品女装" type="text" class="so_kw" data-type="kw" name="kw" value="{if $_GET.kw}{$_GET.kw}{/if}" autocomplete="off" accesskey="s" aria-expanded="true" />
                 </div>
-                <input type="button" class="srh-sub so_web _check_form" id="serarch_button" url="{$URL}a=search" value="搜本站" >
+                <input type="button" class="srh-sub so_web _check_form" id="serarch_button" url="{$URL}a=search" value="开始搜索" >
                 <input type="hidden" value="{$_G.setting.pid}" name="pid">
                 <input type="hidden" value="0" name="search_type">
                 <input type="hidden" value="0" name="search_type_value">
